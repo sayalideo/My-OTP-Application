2314676969647c156bb065b7fc80508c88608891
Saiy <sayalideo137@gmail.com>
2019-03-23 10:58:53 +0530

    OTP working

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var parser     = require("body-parser"),
     router     = require("./routes/index"),
     app        = express();
     
-
+var port = process.env.PORT || 3000;
 const dir      = __dirname;
 app.set('view engine', 'ejs');
 app.use("/public" , express.static(dir + '/public'));
@@ -11,9 +11,8 @@ app.use(parser.urlencoded({extended:true}));
 app.use(parser.json());
 app.use(router);
 
-app.set('port',3000);
 
-var server = app.listen(app.get('port'),()=>{
-    var port = server.address().port;
-    console.log("Magic Happens on port : ",port);
+app.listen(port , function () {
+	console.log('Magic Happens on port : ' + port+'/');
 });
+
